702096342005951f2f6c2cb68e8940c17743628c
Martineau <clement.martineau@etu.u-paris.fr>
2021-11-26 02:27:48 +0100

    use of an ArrayList instead of 3 tile objects in the Road class

--- board/src/board/Road.java
+++ board/src/board/Road.java
@@ -1,67 +1,70 @@
 package board;
 import game.Player;
 
+import java.util.ArrayList;
+
 public class Road {
     protected Player player;
-    protected Tile tile1;
-    protected Tile tile2;
-    protected Tile tile3;
+    protected ArrayList<Tile> tileArrayList=new ArrayList<>();
     public Road(Tile[][] t,int x, int y) {
-        this.tile1=t[x][y];
+        this.tileArrayList.add(t[x][y]);
         this.player=null;
         preciseAttributs(t,x,y);
     }
-
+    /*
+    Fonction qui remplit l'ArrayList de tile correspondant a ses voisins,
+    de manière à pouvoir vérifier s'il y a bien une route voisine lors de la création d'une route.
+     */
     private void preciseAttributs(Tile[][] t,int x, int y) {
         if(x==0){ //ligne du haut
             if(y==0){ //coin gauche
-                if(t[x][y].roads.lastIndexOf(this)==0) this.tile2=t[x][y+1];
-                if(t[x][y].roads.lastIndexOf(this)==3) this.tile2=t[x+1][y];
+                if(t[x][y].roads.lastIndexOf(this)==0) this.tileArrayList.add(t[x][y+1]);
+                if(t[x][y].roads.lastIndexOf(this)==3) this.tileArrayList.add(t[x+1][y]);
             }
             else if(y==t.length){ //coin droit
-                if(t[x][y].roads.lastIndexOf(this)==0) this.tile2=t[x][y-1];
-                if(t[x][y].roads.lastIndexOf(this)==1) this.tile2=t[x+1][y];
-                else this.tile2=t[x+1][y-1];
-            }else this.tile2=t[x+1][y+1];
+                if(t[x][y].roads.lastIndexOf(this)==0) this.tileArrayList.add(t[x][y-1]);
+                if(t[x][y].roads.lastIndexOf(this)==1) this.tileArrayList.add(t[x+1][y]);
+                else this.tileArrayList.add(t[x+1][y-1]);
+            }else this.tileArrayList.add(t[x+1][y+1]);
         }
         else if(x==t.length){// ligne du bas
             if(y==0){ //coin gauche
-                if(t[x][y].roads.lastIndexOf(this)==2) this.tile2=t[x][y+1];
-                else if(t[x][y].roads.lastIndexOf(this)==3) this.tile2=t[x-1][y];
+                if(t[x][y].roads.lastIndexOf(this)==2) this.tileArrayList.add(t[x][y+1]);
+                else if(t[x][y].roads.lastIndexOf(this)==3) this.tileArrayList.add(t[x-1][y]);
             }
             else if(y==t.length){ //coin droit
-                if(t[x][y].roads.lastIndexOf(this)==1) this.tile2=t[x][y-1];
-                else if(t[x][y].roads.lastIndexOf(this)==2) this.tile2=t[x-1][y];
-                else this.tile2=t[x-1][y-1];
-            }else this.tile2=t[x-1][y+1];
+                if(t[x][y].roads.lastIndexOf(this)==1) this.tileArrayList.add(t[x][y-1]);
+                else if(t[x][y].roads.lastIndexOf(this)==2) this.tileArrayList.add(t[x-1][y]);
+                else this.tileArrayList.add(t[x-1][y-1]);
+            }else this.tileArrayList.add(t[x-1][y+1]);
         }
         else if(y==0){ //coté gauche
-            if(t[x][y].roads.lastIndexOf(this)==0) this.tile2=t[x-1][y+1];
+            if(t[x][y].roads.lastIndexOf(this)==0) this.tileArrayList.add(t[x-1][y+1]);
             else if(t[x][y].roads.lastIndexOf(this)==1){ //cas général
-                this.tile2=t[x-1][y+1];
-                this.tile3=t[x+1][y+1];
+                this.tileArrayList.add(t[x-1][y+1]);
+                this.tileArrayList.add(t[x+1][y+1]);
             }
-            else if(t[x][y].roads.lastIndexOf(this)==2) this.tile2=t[x+1][y+1];
+            else if(t[x][y].roads.lastIndexOf(this)==2) this.tileArrayList.add(t[x+1][y+1]);
             else if(t[x][y].roads.lastIndexOf(this)==3){
-                this.tile2=t[x-1][y];
-                this.tile3=t[x+1][y];
+                this.tileArrayList.add(t[x-1][y]);
+                this.tileArrayList.add(t[x+1][y]);
             }
         }
         else if(y==t.length){ //coté droit
-            if(t[x][y].roads.lastIndexOf(this)==0) this.tile2=t[x-1][y-1];
+            if(t[x][y].roads.lastIndexOf(this)==0) this.tileArrayList.add(t[x-1][y-1]);
             else if(t[x][y].roads.lastIndexOf(this)==1){
-                this.tile2=t[x-1][y];
-                this.tile3=t[x+1][y];
+                this.tileArrayList.add(t[x-1][y]);
+                this.tileArrayList.add(t[x+1][y]);
             }
-            else if(t[x][y].roads.lastIndexOf(this)==2) this.tile2=t[x+1][y-1];
+            else if(t[x][y].roads.lastIndexOf(this)==2) this.tileArrayList.add(t[x+1][y-1]);
             else if(t[x][y].roads.lastIndexOf(this)==3){
-                this.tile2=t[x-1][y-1];
-                this.tile3=t[x+1][y-1];
+                this.tileArrayList.add(t[x-1][y-1]);
+                this.tileArrayList.add(t[x+1][y-1]);
             }
         }
         else { //cas général
-            this.tile2=t[x-1][y+1];
-            this.tile3=t[x+1][y+1];
+            this.tileArrayList.add(t[x-1][y+1]);
+            this.tileArrayList.add(t[x+1][y+1]);
         }
     }
 }
