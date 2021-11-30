2ee0463dc1c82713b4735451b68356791b4a55d0
ryoh <lucrod1000@gmail.com>
2021-12-01 00:02:04 +0100

    Add Human class and methods to generate a player

--- game/src/game/Player.java
+++ game/src/game/Player.java
@@ -1,5 +1,17 @@
 package game;
 
-public abstract class Player{
+import java.util.HashMap;
 
+public abstract class Player{
+    protected String color;
+    protected HashMap<String,Integer> ressources;res
+    protected int victoryPoint=0;
+
+    protected void fillRessourcesMap(){
+        ressources.put("Clay",0);
+        ressources.put("Ore",0);
+        ressources.put("Wheat",0);
+        ressources.put("Wood",0);
+        ressources.put("Wool",0);
+    }
 }
